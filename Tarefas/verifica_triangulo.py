7856b81311920663d0c739343d2c3b28349cb446
Adam Jamal <adam.jamal@grad.ufsc.br>
2022-09-28 20:05:31 -0300

    Tarefas

--- Tarefas/verifica_triangulo.py
+++ Tarefas/verifica_triangulo.py
@@ -1,7 +1,3 @@
-#Faça um programa em Python que leia 3 valores correspondentes aos lados de um triângulo e escreva True se o triângulo 
-##formado por esses 3 lados é equilátero; False caso contrário. Note que o programa não deve escrever nada além disso.
-###Na leitura, também não deverá ser escrito textos pedindo para o usuário digitar os lados, tipo "Entre com o lado 1".
-
 lado1 = float(input(""))
 lado2 = float(input(""))
 lado3 = float(input(""))
